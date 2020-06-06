cb48ce27253e3405ad997dbb0f5f37b03c24dd5b
Diogo Machado <diogomachado_8@hotmail.com>
2020-06-06 06:57:28 -0300

    fix.Event codintions

--- src/app/controllers/WatcherDetailController.ts
+++ src/app/controllers/WatcherDetailController.ts
@@ -16,18 +16,16 @@ class WatcherController {
     if (query.month) {
       month = parseInt(query.month as string, 10);
 
-      if (query.month && !month && month <= 12 && month >= 1)
+      if (!month && month <= 12 && month >= 1)
         return res.status(400).json({ message: 'Invalid Month' });
     }
 
     if (query.year) {
       year = parseInt(query.year as string, 10);
 
-      if (query.year && !year)
-        return res.status(400).json({ message: 'Invalid Year' });
+      if (!year) return res.status(400).json({ message: 'Invalid Year' });
     }
 
-    console.log(month, year);
     const item = await this.service.getByIdWithEvent(id, userId, month, year);
 
     return res.status(200).json(item);
