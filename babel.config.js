8f2c5d173dccd001664bed4b23329e19cd45202c
Diogo Machado <diogomachado_8@hotmail.com>
2020-07-20 07:11:19 -0300

    fix/hbs

--- babel.config.js
+++ babel.config.js
@@ -1,13 +1,28 @@
 module.exports = {
-  plugins: ['@babel/plugin-proposal-optional-chaining'],
   presets: [
     [
       '@babel/preset-env',
       {
         targets: {
-          node: 'current',
-        },
-      },
+          node: 'current'
+        }
+      }
     ],
+    '@babel/preset-typescript'
   ],
-};
+  plugins: [
+    ['module-resolver', {
+      alias: {
+        '@tests': './src/__tests__',
+        '@app': './src/app',
+        '@config': './src/config',
+        '@database': './src/database',
+        '@lib': './src/lib',
+        '@src': './src'
+      }
+    }]
+  ],
+  ignore: [
+    '**/*.spec.ts'
+  ]
+}
